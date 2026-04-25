7cd45974fc35090a7e033490c9db49fbef735203
sepinedao <sepinedao@unal.edu.co>
2026-04-25 14:11:05 -0500

    Update pregunta_05.py

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -10,7 +10,7 @@ def pregunta_05():
 
     resultado = []
 
-    with open ('files\input\data.csv', 'r') as file:
+    with open ('files/input/data.csv', 'r') as file:
         for line in file:
             columnas = line.strip().split('\t')
             clave = columnas[0]
